6618ba50436dd05292847a81da8f93c46fa51398
TIKITA PERALTA <tikitaperalta@gmail.com>
2024-03-15 23:16:01 +0000

    puppeteer bun works screenshot test

--- scrape.js
+++ scrape.js
@@ -1,1 +1,18 @@
-URL = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E94019&insId=1&radius=0.25&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='
+//URL = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=REGION%5E94019&insId=1&radius=0.25&minPrice=&maxPrice=&minBedrooms=&maxBedrooms=&displayPropertyType=&maxDaysSinceAdded=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare='
+
+//adding Puppeteer library
+import { launch } from 'puppeteer';
+launch().then(async browser => {
+//browser new page
+const p = await browser.newPage();
+//set viewpoint of browser page
+await p.setViewport({ width: 1000, height: 500 })
+//launch URL
+await p.goto('https://bstackdemo.com')
+//capture screenshot
+await p.screenshot({
+path: 'browserstack-demo.png'
+});
+//browser close
+await browser.close()
+})
